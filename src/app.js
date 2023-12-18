068f2d5223eab23ece40c315262e433901260ca7
henriqueAvner <avnerhdpb@gmail.com>
2023-12-18 14:22:08 -0300

    Fixed: Alterando frase da página inicial do endpoint

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', (_req, res) => res
 .status(200)
-.json({ message: 'CRUD employees! Type /funcionarios to bring all employees!' }));
+.json({ message: 'CRUD employees!! Type /funcionarios to bring all employees!' }));
 
 app.get('/funcionarios', async (_req, res) => {
     try {

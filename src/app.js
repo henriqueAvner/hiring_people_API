a16300d7307a8901b405dadb27125602cb2a4d96
henriqueAvner <avnerhdpb@gmail.com>
2023-12-18 16:11:52 -0300

    Feat: Criando o método POST para a adição de novos funcionários

--- src/app.js
+++ src/app.js
@@ -1,21 +1,23 @@
 const express = require('express');
-const { readEmployees, findEmployee } = require('./middlewares/functions');
+const { readEmployees, findEmployee, addEmployee } = require('./middlewares/functions');
 
 const app = express();
+app.use((express.json()));
 
 app.get('/', (_req, res) => res
 .status(200)
 .json({ message: 'CRUD employees!! Type /funcionarios to bring all employees!' }));
-
+// Retorna todos os funcionários da empresa: 
 app.get('/funcionarios', async (_req, res) => {
     try {
       const allEmployees = await readEmployees();
-      return res.status(200).json(allEmployees);
+      return res.status(200).json({ employees: allEmployees });
     } catch (error) {
       return res.status(404).send(error.message);  
     }
 });
 
+// Retorna o funcionário da empresa pelo ID:
 app.get('/funcionarios/:id', async (req, res) => {
     const { id } = req.params;
     const currEmployee = await findEmployee(Number(id));
@@ -23,7 +25,17 @@ app.get('/funcionarios/:id', async (req, res) => {
  return res
     .status(404).json({ message: 'Employee not found' }); 
 }
-    res.status(200).json({ currEmployee });
+    res.status(200).json(currEmployee);
+});
+// Adiciona um novo funcionário a empresa:
+app.post('/funcionarios', async (req, res) => {
+   try {
+    const { nome, cargo, departamento, salario } = req.body;
+    await addEmployee(nome, cargo, departamento, salario);
+    return res.status(201).json({ message: 'Employee added!' });
+   } catch (error) {
+    return res.status(500).send({ message: error.message });
+   }
 });
 
 module.exports = app;

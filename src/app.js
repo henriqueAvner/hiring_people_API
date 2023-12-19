6a739af571b0221fdb3c8c2c23377bbeac7e90b7
henriqueAvner <avnerhdpb@gmail.com>
2023-12-19 16:51:15 -0300

    Feat: Método PUT: Implementando método para alteração de dados de funcionário

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
+
 const { readEmployees, findEmployee, addEmployee } = require('./middlewares/functions');
+
+const employeesPath = path.resolve(__dirname, './files/employees.json');
 
 const app = express();
 app.use((express.json()));
@@ -36,6 +41,28 @@ app.post('/funcionarios', async (req, res) => {
    } catch (error) {
     return res.status(500).send({ message: error.message });
    }
+});
+
+// Editando um funcionário da empresa:
+
+app.put('/funcionarios/:id', async (req, res) => {
+  const { id } = req.params;
+  const { nome, cargo, departamento, salario } = req.body;
+  const allEmployees = await readEmployees();
+  const updateCurrEmp = allEmployees.find((employee) => employee.id === +id);
+  if (!updateCurrEmp) {
+    return res.status(404).json({ message: 'Employee not found' });
+  }
+
+  updateCurrEmp.nome = nome;
+  updateCurrEmp.cargo = cargo;
+  updateCurrEmp.departamento = departamento;
+  updateCurrEmp.salario = salario;
+
+  const newEmployee = JSON.stringify(allEmployees);
+  await fs.writeFile(employeesPath, newEmployee);
+
+  return res.status(200).json(updateCurrEmp);
 });
 
 module.exports = app;

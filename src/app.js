5194bf6a784b86b30f50ca4de2c1e6c4e28340d0
henriqueAvner <avnerhdpb@gmail.com>
2023-12-20 16:44:23 -0300

    Feat: Adicionando método GET: Retornando funcionários pelo departamento

--- src/app.js
+++ src/app.js
@@ -12,7 +12,9 @@ app.use((express.json()));
 app.get('/', (_req, res) => res
 .status(200)
 .json({ message: 'CRUD employees!! Type /funcionarios to bring all employees!' }));
-// Retorna todos os funcionários da empresa: 
+
+// Retorna todos os funcionários da empresa:
+
 app.get('/funcionarios', async (_req, res) => {
     try {
       const allEmployees = await readEmployees();
@@ -22,7 +24,26 @@ app.get('/funcionarios', async (_req, res) => {
     }
 });
 
+// Retorna todos os funcionários baseados em seu departamento:
+
+app.get('/funcionarios/search', async (req, res) => {
+  const { dep } = req.query;
+  const allEmployees = await readEmployees();
+  if (!dep) {
+    return res.status(200).json(allEmployees);
+  }
+  const filterEmployees = allEmployees
+  .filter(({ departamento }) => departamento
+  .toLowerCase() === (dep.toLowerCase()));
+
+  if (filterEmployees.length === 0) {
+    return res.status(200).json([]);
+  }
+  return res.status(200).json(filterEmployees);
+});
+
 // Retorna o funcionário da empresa pelo ID:
+
 app.get('/funcionarios/:id', async (req, res) => {
     const { id } = req.params;
     const currEmployee = await findEmployee(Number(id));
@@ -32,7 +53,9 @@ app.get('/funcionarios/:id', async (req, res) => {
 }
     res.status(200).json(currEmployee);
 });
+
 // Adiciona um novo funcionário a empresa:
+
 app.post('/funcionarios', async (req, res) => {
    try {
     const { nome, cargo, departamento, salario } = req.body;
@@ -53,23 +76,20 @@ app.put('/funcionarios/:id', async (req, res) => {
   if (!updateCurrEmp) {
     return res.status(404).json({ message: 'Employee not found' });
   }
-
   updateCurrEmp.nome = nome;
   updateCurrEmp.cargo = cargo;
   updateCurrEmp.departamento = departamento;
   updateCurrEmp.salario = salario;
-
   const newEmployee = JSON.stringify(allEmployees);
   await fs.writeFile(employeesPath, newEmployee);
-
   return res.status(200).json(updateCurrEmp);
 });
+
 // Deletando um funcionário através de seu index: 
 
 app.delete('/funcionarios/:id', async (req, res) => {
   const { id } = req.params;
   const allEmployees = await readEmployees();
-
   const currEmployee = allEmployees.findIndex((employee) => employee.id === +id);
   if (currEmployee === -1) {
     return res.status(404).json({ message: 'Employee not found!' });
